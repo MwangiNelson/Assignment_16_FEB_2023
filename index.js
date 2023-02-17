737583a609738d924638f5b762ae677d2e161cdb
MwangiNelson <nelsonmwangi197@gmail.com>
2023-02-17 16:00:34 +0300

    YOLO

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
-console.log("------------------------------------- SOLUTION 1 --------------------------------------------------")
 //printing even numbers
+console.log("------------------------------------- SOLUTION 1 --------------------------------------------------")
+
 function printEvenNumbers(range) {
     let start = range[0]
     let end = range[1]
@@ -12,10 +12,9 @@ function printEvenNumbers(range) {
 }
 printEvenNumbers([0,10])
 
-
+//printing multiple tables
 console.log("------------------------------------- SOLUTION 2 --------------------------------------------------")
 
-//printing multiple tables
 function getMultiple(range) {
     let start = range[0]
     let end = range[range.length - 1]
@@ -30,21 +29,25 @@ function getMultiple(range) {
         all_multiples_table.push(table)
     }
 
-    return all_multiples_table
+    console.table(all_multiples_table)
 }
-console.table(getMultiple([1, 10]))
+getMultiple([1, 10])
 
 
-console.log("------------------------------------- SOLUTION 3 --------------------------------------------------")
 //length converter
+console.log("------------------------------------- SOLUTION 3 --------------------------------------------------")
+
 let kms = 10
+
 function Km_To_Miles_Converter(km){
     console.log(`${km} kilometers = ${km*0.621371} miles`)
 }
 Km_To_Miles_Converter(kms)
 
-console.log("------------------------------------- SOLUTION 4 --------------------------------------------------")
+
 //sum of nums in array
+console.log("------------------------------------- SOLUTION 4 --------------------------------------------------")
+
 let array = [1,2,3,4,5,6,7,8,9,10]
 function getSumArr(arr){
     let sum = arr.reduce((accumulator, currentVal) => accumulator + currentVal)
